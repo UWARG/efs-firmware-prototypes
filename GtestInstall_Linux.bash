105ba317702533bce03ddb52b25f0540f24841c4
DerekTang04 <tang.derek04@gmail.com>
2023-06-29 01:47:59 -0400

    original files

---
+++ GtestInstall_Linux.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir GtestTemp
+cd GtestTemp
+
+curl -LO https://github.com/google/googletest/archive/release-1.10.0.tar.gz
+tar xf release-1.10.0.tar.gz
+cd googletest-release-1.10.0
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+cd ../../..
+rm -r GtestTemp
